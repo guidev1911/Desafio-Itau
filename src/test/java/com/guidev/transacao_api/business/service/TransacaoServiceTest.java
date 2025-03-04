362cb378f205a8dde30bf3c409c1eeec3bf0e2a4
guidev1911 <guilhermereal1911@gmail.com>
2025-03-03 21:46:06 -0300

    testes para transação

---
+++ src/test/java/com/guidev/transacao_api/business/service/TransacaoServiceTest.java
@@ -0,0 +1,81 @@
+package com.guidev.transacao_api.business.service;
+
+import com.guidev.transacao_api.business.services.TransacaoService;
+import com.guidev.transacao_api.controller.dtos.EstatisticasResponseDTO;
+import com.guidev.transacao_api.controller.dtos.TransacaoRequestDTO;
+import com.guidev.transacao_api.infrastructure.exceptions.UnprocessableEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TransacaoServiceTest {
+
+    @InjectMocks
+    TransacaoService transacaoService;
+    TransacaoRequestDTO transacao;
+
+    EstatisticasResponseDTO estatisticas;
+    @BeforeEach
+    void setUp(){
+        transacao = new TransacaoRequestDTO(20.0, OffsetDateTime.now());
+        estatisticas = new EstatisticasResponseDTO(1L, 20.0, 20.0, 20.0, 20.0);
+    }
+
+    @Test
+    void deveAdicionarTransacaoComSucesso(){
+
+        transacaoService.adicionarTransacoes(transacao);
+
+        List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(5000);
+        assertTrue(transacoes.contains(transacao));
+    }
+    @Test
+    void deveLancarExcecaoCasoValorSejaNegativo(){
+
+        UnprocessableEntity exception = assertThrows(UnprocessableEntity.class,
+                () -> transacaoService.adicionarTransacoes(new TransacaoRequestDTO(-10.0, OffsetDateTime.now())));
+
+        assertEquals("valor não pode ser menor que 0", exception.getMessage());
+    }
+    @Test
+    void deveLancarExcecaoCasoDataOuHoraMaiorQueAtual(){
+
+        UnprocessableEntity exception = assertThrows(UnprocessableEntity.class,
+                () -> transacaoService.adicionarTransacoes(new TransacaoRequestDTO(10.0, OffsetDateTime.now().plusDays(1))));
+
+        assertEquals("Data e hora maiores que a atual", exception.getMessage());
+    }
+
+    @Test
+    void deveLimparTransacaoComSucesso(){
+
+        transacaoService.limparTransacoes();
+
+        List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(5000);
+
+        assertTrue(transacoes.isEmpty());
+    }
+
+    @Test
+    void deveBuscarTransacaoDentroDoIntervalo(){
+
+        TransacaoRequestDTO dto = new TransacaoRequestDTO(10.00, OffsetDateTime.now().minusHours(1));
+
+        transacaoService.adicionarTransacoes(transacao);
+        transacaoService.adicionarTransacoes(dto);
+
+        List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(60);
+
+        assertFalse(transacoes.contains(dto));
+        assertTrue(transacoes.contains(transacao));
+    }
+
+}
